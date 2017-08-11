0da408d71137d44e2ccfb12f3f9803a2083f9aa3
Salvador Torres <ssatorres@gmail.com>
2017-08-11 11:44:05 -0300

    Commit inicial

---
+++ api/config.php
@@ -0,0 +1,10 @@
+<?php
+define('SERVER', 'D');
+define('MYSQL_HOST', 'localhost');
+define('MYSQL_PORT', '3306');
+define('MYSQL_USER', 'selecao_phpdev');
+define('MYSQL_PASS', 'barcelona');
+define('MYSQL_BASE', 'selecao_phpdev2017_2');
+define('MYSQL_CHARSET', 'utf8');
+define('MYSQL_TIMEZONE', '-03:00');
+define('PHP_TIMEZONE', 'America/Bahia');
